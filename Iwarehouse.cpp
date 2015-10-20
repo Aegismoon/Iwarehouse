f3d7034b922b5a392a3363a2a70873eff611a4f2
aegismoon <aegismoon@yandex.ru>
2015-10-20 23:03:52 +0500

    first stable program subversion. (  input show method call)
    infinite mechanis is on if sequences - switch by enum required

--- Iwarehouse.cpp
+++ Iwarehouse.cpp
@@ -15,141 +15,289 @@
 using namespace std;
 
 // numeral for purposes
-enum measureUnits  { kg,ft,m,pcs };
-enum currensyUnits { rub,usd,eur };
-enum commandList   { help, sort, add, clear, save, load, close };
+// kg-kilogramm, ft -feet, m - meters, pcs - pcs
+enum measure    { kg,ft,m,pcs };
+// rub - rubles, usd - US dollars, eur - euro
+enum currensy   { rub,usd,eur };
+// list of command available for user
+enum commands   { help, sort, add, clear, save, load, close };
 
 // long story short
-typedef measureUnits meU;
 typedef unsigned int UINT;
-typedef currensyUnits cuU;
-typedef commandList comLi;
+
 
 // structrure for storage
-struct storeItem
-	{
-		double price;
-		string name;
-		UINT number;
-		UINT amount;
-		meU	   itemUnit;
-		cuU	   itemCurrency;
-//		expansion case - futuremark
-//		char   *managerName;
-		char   *itemName;
-		bool   avail;
-	};
+struct store
+{
+    double price;
+    string title;
+    UINT keycode;
+    UINT amount;
+    measure    Unit;
+    currensy   Currency;
+//	futuremark
+//	string	 managerName;
+    bool   available;
+};
 
-typedef storeItem stI;
 
-// function prototypespes
-void  addFunc () ;
-void  sortFunc() ;
-void  helpFunc() ;
-void  closeFunc();
-void  saveFunc() ;
-void  loadFunc() ;
-void  clearFunc();
+
+// function prototypes
+namespace   Murat
+{
+
+void  add () ;
+void  sort() ;
+void  help() ;
+void  close();
+void  save() ;
+void  load() ;
+void  clear();
+void loop();
+}
+
 
 
 int main (int argc, char* argv[])
 
 {
-//  console app header 3 t on the right
-    cout << "\t\t\t Warehouse program" << '\n';
+
 
 //  setting request string
+    Murat::loop();
 
- 
-   loopFunc();
-
-   return 0;
+    return 0;
 }
 
 
 
-void loopFunc (string command)
+void loop ()
 {
+    //  console app header 3 t on the right
+    cout << "\t\t\t Warehouse program" << '\n';
+    cout << "\nEnter your command please>";
 
-	cout << "\nEnter your command please>";
 
-	// infinite loop
-	while (1)
-	{	
-		string command;
-		
-		if (cin >> command)
-		{
-		//	check wich command is written
-		// elegant solution for string to enum required	
-			
-		    switch (switcher)
-			{
-				case  add  :  addFunc();   loopFunc();
-				case  sort :  sortFunc();   loopFunc();
-				case  clear:  clearFunc();   loopFunc();
-				case  help :  helpFunc();   loopFunc();
-				case  close:  closeFunc();	loopFunc();
-				case  save :  saveFunc();    loopFunc();
-				case  load :  loadFunc();   loopFunc();;
-				default: cout << "\nThis causes no effect in program, use >help for assistance"; loopFunc();
-				// also recursive solution required without stack overflow
-				
-			}// switch breaks
-		} // command enter condition break
+    while (1)
+    {
+        string command;
+        // to catch CLRF
+        string crlf = ("'\n'");
 
-	}// infinite loop break
+        if (cin >> command)
+        {
+            // elegant solution for string to enum required
+
+            // conditions to check - very unaccurate need to be refactored
+
+
+
+            if ( command == "add" )
+            {
+                Murat::add();
+
+                Murat::loop();
+            }
+
+            if ( command == "clear" )
+            {
+                Murat::clear();
+
+               Murat::loop();
+            }
+
+            if (command == "sort" )
+            {
+                Murat::sort();
+
+               Murat::loop();
+            }
+            if (command == "help" )
+            {
+                Murat::help();
+
+               Murat::loop();
+            }
+            if (command == "close" )
+            {
+                Murat::close();
+
+                Murat::loop();
+            }
+            if ( command == "save" )
+            {
+                Murat::save();
+
+               Murat::loop();
+            }
+            if (command == "load" )
+            {
+                Murat::load();
+
+               Murat::loop();
+            }
+
+
+            /*
+              switch (switcher)
+            {
+            	case  add  :  Murat::add();    Murat::loop();
+            	case  sort :  Murat::sort();   Murat::loop();
+            	case  clear:  Murat::clear();  Murat::loop();
+            	case  help :  Murat::help();   Murat::loop();
+            	case  close:  Murat::close();  Murat::loop();
+            	case  save :  Murat::save();   Murat::loop();
+            	case  load :  Murat::load();   Murat::loop();
+            	default: cout << "\nThis causes no effect in program, use >help for assistance"; Murat::loop();
+            	// also recursive solution required without stack overflow
+
+            }// switch breaks are for experiments */
+        } // command enter condition break
+
+    }// infinite loop break
 
 
 }
 
-void addFunc()
-{
-  cout << "addFunction" << '\n';
-  system ("pause");
-  system ("cls");
-};
 
-void sortFunc()
-{
-  cout << "sortFunc" << '\n';
-  system ("pause");
-  system ("cls");
-};
 
-void clearFunc()
-{
-  cout << "clearFunc" << '\n';
-  system ("pause");
-  system ("cls");
-};
 
-void helpFunc()
-{
-  cout << "helpFunc" << '\n';
-  system ("pause");
-  system ("cls");
-};
 
-void closeFunc()
+void add()
 {
-  cout << "closeFunc" << '\n';
-  system ("pause");
-  system ("cls");
-};
+    cout << "addFunction" << '\n';
+    system ("pause");
+    system ("cls");
+}
 
-void saveFunc()
+void sort()
 {
-  cout << "saveFunc" << '\n';
-  system ("pause");
-  system ("cls");
-};
+    cout << "sortFunc" << '\n';
+    system ("pause");
+    system ("cls");
+}
 
-void loadFunc()
+void clear()
 {
-  cout << "loadFunc" << '\n';
-  system ("pause");
-  system ("cls");
-};
+    cout << "clearFunc" << '\n';
+    system ("pause");
+    system ("cls");
+}
+
+void help()
+{
+    cout << "helpFunc" << '\n';
+    system ("pause");
+    system ("cls");
+}
+
+void close()
+{
+    cout << "closeFunc" << '\n';
+    system ("pause");
+    system ("cls");
+}
+
+void save()
+{
+    cout << "saveFunc" << '\n';
+    system ("pause");
+    system ("cls");
+}
+
+void load()
+{
+    cout << "loadFunc" << '\n';
+    system ("pause");
+    system ("cls");
+}
+
+void loop ()
+{
+    //  console app header 3 t on the right
+    cout << "\t\t\t Warehouse program" << '\n';
+    cout << "\nEnter your command please>";
+
+
+    while (1)
+    {
+        string command;
+
+        if (cin >> command)
+        {
+            // elegant solution for string to enum required
+
+            // conditions to check - very unaccurate need to be refactored
+
+
+
+            if ( command == "add" )
+            {
+                Murat::add();
+
+              Murat::loop();
+            }
+
+            if ( command == "clear" )
+            {
+                Murat::clear();
+
+                Murat::loop();
+            }
+
+            if (command == "sort" )
+            {
+                Murat::sort();
+
+                Murat::loop();
+            }
+            if (command == "help" )
+            {
+                Murat::help();
+
+               Murat::loop();
+            }
+            if (command == "close" )
+            {
+                Murat::close();
+
+                Murat::loop();
+            }
+            if ( command == "save" )
+            {
+                Murat::save();
+
+               Murat::loop();
+            }
+            if (command == "load" )
+            {
+                Murat::load();
+
+               Murat::loop();
+            }
+
+
+            /*
+              switch (switcher)
+            {
+            	case  add  :  addFunc();    loopFunc();
+            	case  sort :  sortFunc();   loopFunc();
+            	case  clear:  clearFunc();  loopFunc();
+            	case  help :  helpFunc();   loopFunc();
+            	case  close:  closeFunc();	loopFunc();
+            	case  save :  saveFunc();   loopFunc();
+            	case  load :  loadFunc();   loopFunc();
+            	default: cout << "\nThis causes no effect in program, use >help for assistance"; loopFunc();
+            	// also recursive solution required without stack overflow
+
+            }// switch breaks are for experiments */
+        } // command enter condition break
+
+    }// infinite loop break
+
+
+}
+
+
 
 
